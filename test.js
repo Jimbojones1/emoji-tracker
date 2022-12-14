8d322fb7b803614dd93bbce2af44780eee3cfd46
JimHaff <james.haff@generalassemb.ly>
2022-12-13 20:54:20 -0600

    added emoji tracker

---
+++ test.js
@@ -0,0 +1,40 @@
+{
+	ok: true,
+	messages: [
+	  {
+		client_msg_id: 'd01e8251-f128-492b-8f8d-2c00ca7e7d41',
+		type: 'message',
+		text: 'no Emoji on message',
+		user: 'ULN87HENL',
+		ts: '1647467433.818739',
+		team: 'T0KQ14SDP',
+		blocks: [Array]
+	  }
+	],
+	has_more: true,
+	is_limited: false,
+	pin_count: 0,
+	channel_actions_ts: null,
+	channel_actions_count: 0,
+	response_metadata: {
+	  next_cursor: 'bmV4dF90czoxNjQ3NDY3MzIwMDQ3OTY5',
+	  scopes: [
+		'channels:history',
+		'reactions:read',
+		'emoji:read',
+		'groups:history',
+		'im:history',
+		'chat:write',
+		'im:read',
+		'users.profile:read'
+	  ],
+	  acceptedScopes: [
+		'channels:history',
+		'groups:history',
+		'mpim:history',
+		'im:history',
+		'read'
+	  ]
+	}
+  }
+  
